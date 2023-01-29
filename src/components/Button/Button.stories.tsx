bf073c9f528fe694d25e0a387ed10a32b84bcda8
Ernesto0G <ernesto7gutierrez@gmail.com>
2023-01-29 11:54:33 -0800

    Added more parameters for styling buttons

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -6,19 +6,43 @@ import Button from "./Button";
 export default {
     title: "ReactComponentLibrary/Button",
     component: Button,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+        borderColor: { control: 'color' },
+        color: { control: 'color' },
+    },
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const HelloWorld = Template.bind({});
+export const Modern = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-HelloWorld.args = {
+Modern.args = {
     label: "Hello world!",
     className: 'button-modern',
 };
 
-export const ClickMe = Template.bind({});
-ClickMe.args = {
-    label: "Click me!",
+export const Minimal = Template.bind({});
+Minimal.args = {
+    label: "Hello world!",
+    className: 'button-minimal',
+};
+
+export const FillSmall = Template.bind({});
+FillSmall.args = {
+    label: "Hello world!",
+    className: 'button-minimal button-fill-sml',
+};
+
+export const FillMedium = Template.bind({});
+FillMedium.args = {
+    label: "Hello world!",
+    className: 'button-minimal button-fill-med',
+};
+
+export const FillLarge = Template.bind({});
+FillLarge.args = {
+    label: "Hello world!",
+    className: 'button-minimal button-fill-lar',
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,11 +5,26 @@ import "./Button.css"
 export interface ButtonProps {
     label: string;
     className: string;
+    backgroundColor?: string;
+    borderColor?: string;
+    color?: string;
+    size?: [number, number];
+    theme?: string;
 }
 
 const Button = (props: ButtonProps) => {
     return (
-        <button className={props.className}>{props.label}</button>
+        <button
+            type='button'
+            className={props.className}
+            style={{
+                backgroundColor: props.backgroundColor,
+                borderColor: props.borderColor,
+                color: props.color
+            }}
+        >
+            {props.label}
+        </button >
     )
 }
 
